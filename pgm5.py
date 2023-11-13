9ed3ec60d45c4002dfedef10258aa99191b5178b
Amal Tom Parakkaden <74197006+amaltom06@users.noreply.github.com>
2023-11-13 10:44:35 +0530

    Update pgm5.py

--- pgm5.py
+++ pgm5.py
@@ -1,4 +1,5 @@
 import csv
+import pandas as pd
 field=['rollno','name','age']
 sdict=[{'rollno':7,'name':"amal",'age':37},
        {'rollno':10,'name':"tom",'age':34}]
@@ -6,9 +7,8 @@ with open("vpt.csv",'w')as dfile:
      writer=csv.DictWriter(dfile,fieldnames=field)
      writer.writeheader()
      writer.writerows(sdict)
-with open("vpt.csv","r") as e_file:  
-     data=csv.reader(e_file)
-     for i in data:
-      print(i)    
+data=pd.read_csv("vpt.csv")   
+print(data)  
+   
      
 
